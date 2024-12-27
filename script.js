63004a8cfb6c3e5d4e7deea76eb8d45c8c2aa6c4
yuri-italo <yuri.italo94@gmail.com>
2024-12-27 14:01:52 -0300

    feat: add displayCard method

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const myLibrary = [];
 
+initialData();
+displayCards(myLibrary);
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -15,26 +18,81 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-const book1 = new Book("1984", "George Orwell", 328, true);
-const book2 = new Book("To Kill a Mockingbird", "Harper Lee", 281, false);
-const book3 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
-const book4 = new Book("Moby-Dick", "Herman Melville", 635, false);
-const book5 = new Book("Pride and Prejudice", "Jane Austen", 279, true);
-const book6 = new Book("The Catcher in the Rye", "J.D. Salinger", 214, false);
-const book7 = new Book("The Hobbit", "J.R.R. Tolkien", 310, true);
-const book8 = new Book("Fahrenheit 451", "Ray Bradbury", 158, false);
-const book9 = new Book("Jane Eyre", "Charlotte Brontë", 500, true);
-const book10 = new Book("Brave New World", "Aldous Huxley", 268, false);
+function displayCards(books) {
+  const container = document.querySelector(".main-container");
 
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
-addBookToLibrary(book4);
-addBookToLibrary(book5);
-addBookToLibrary(book6);
-addBookToLibrary(book7);
-addBookToLibrary(book8);
-addBookToLibrary(book9);
-addBookToLibrary(book10);
+  books.forEach((book) => {
+    const bookCard = createBookCard(
+      book.title,
+      book.author,
+      book.pages,
+      book.read
+    );
 
-console.log(myLibrary);
+    container.appendChild(bookCard);
+  });
+}
+
+function createBookCard(title, author, pages, readStatus) {
+  const card = document.createElement("div");
+  card.classList.add("book-card");
+
+  const bookTitle = document.createElement("h2");
+  bookTitle.classList.add("book-title");
+  bookTitle.textContent = title;
+
+  const bookInfo = document.createElement("div");
+  bookInfo.classList.add("book-info");
+
+  const bookAuthor = document.createElement("p");
+  bookAuthor.classList.add("book-author");
+  bookAuthor.textContent = `Author: ${author}`;
+
+  const bookPages = document.createElement("p");
+  bookPages.classList.add("book-pages");
+  bookPages.textContent = `Pages: ${pages}`;
+
+  const bookRead = document.createElement("p");
+  bookRead.classList.add("book-read");
+  bookRead.innerHTML = `Read: <span>${readStatus ? "Yes" : "No"}</span>`;
+
+  bookInfo.appendChild(bookAuthor);
+  bookInfo.appendChild(bookPages);
+  bookInfo.appendChild(bookRead);
+
+  const cardButtons = document.createElement("div");
+  cardButtons.classList.add("card-buttons");
+
+  const statusButton = document.createElement("button");
+  statusButton.classList.add("btn");
+  statusButton.textContent = "Status";
+
+  const removeButton = document.createElement("button");
+  removeButton.classList.add("btn");
+  removeButton.textContent = "Remove";
+
+  cardButtons.appendChild(statusButton);
+  cardButtons.appendChild(removeButton);
+
+  card.appendChild(bookTitle);
+  card.appendChild(bookInfo);
+  card.appendChild(cardButtons);
+
+  return card;
+}
+
+function initialData() {
+  const book1 = new Book("1984", "George Orwell", 328, true);
+  const book2 = new Book("To Kill a Mockingbird", "Harper Lee", 281, false);
+  const book3 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
+  const book4 = new Book("Moby-Dick", "Herman Melville", 635, false);
+  const book5 = new Book("Pride and Prejudice", "Jane Austen", 279, true);
+  const book6 = new Book("The Catcher in the Rye", "J.D. Salinger", 214, false);
+
+  addBookToLibrary(book1);
+  addBookToLibrary(book2);
+  addBookToLibrary(book3);
+  addBookToLibrary(book4);
+  addBookToLibrary(book5);
+  addBookToLibrary(book6);
+}
